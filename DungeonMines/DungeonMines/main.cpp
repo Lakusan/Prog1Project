0db6104d1767fbfa3f76267608988a82720bd863
Lakusan <47719054+Lakusan@users.noreply.github.com>
2019-05-03 09:04:47 +0200

    Collision Keybardinput

--- DungeonMines/DungeonMines/main.cpp
+++ DungeonMines/DungeonMines/main.cpp
@@ -9,9 +9,9 @@ typedef struct {
 }box;
 
 typedef struct {
-	int score=0;
-	int posx=220;
-	int posy=220;
+	int score = 0;
+	int posx = 220;
+	int posy = 220;
 	Vector2 vPos = { 220,220 };
 	int speed = 5;
 }Player;
@@ -31,10 +31,10 @@ int main(int argc, char* argv[])
 	SetTargetFPS(60);
 
 	//init levels
-	Rectangle Boundarys = { 0,0,(float)(GetScreenWidth()),(float)(GetScreenHeight())};
+	Rectangle Boundarys = { 0,0,(float)(GetScreenWidth()),(float)(GetScreenHeight()) };
 	Rectangle Boundarys_innen = { 120,120,(float)(GetScreenWidth()),(float)(GetScreenHeight()) };
 
-	Rectangle PlayerHitBox = {220,220,120,120};
+	Rectangle PlayerHitBox = { 220,220,120,120 };
 
 	//--------------------------------------------------------------------------------------
 
@@ -50,11 +50,11 @@ int main(int argc, char* argv[])
 
 		if (IsKeyDown(KEY_RIGHT))
 		{
-			if(player1.posx <= ((GetScreenWidth()) - (2*(GetScreenWidth() / 32))))
+			if (player1.posx < ((GetScreenWidth()) - (2 * (GetScreenWidth() / 32))))
 			{
-				std::cout << player1.posx << std::endl;
-				std::cout << player1.posy << std::endl;
-			
+				/*	std::cout << player1.posx << std::endl;
+					std::cout << player1.posy << std::endl;*/
+
 
 				player1.posx += player1.speed;
 				PlayerHitBox.x = player1.posx;
@@ -62,36 +62,47 @@ int main(int argc, char* argv[])
 		}
 		if (IsKeyDown(KEY_LEFT))
 		{
-			player1.posx -= player1.speed;
-			PlayerHitBox.x = player1.posx;
+			if (player1.posx > (GetScreenWidth() / 32))
+			{
+				player1.posx -= player1.speed;
+				PlayerHitBox.x = player1.posx;
+			}
 		}
 		if (IsKeyDown(KEY_UP))
 		{
-			player1.posy -= player1.speed;
-			PlayerHitBox.y = player1.posy;
+			if (player1.posy > (GetScreenHeight() / 18))
+			{
+				player1.posy -= player1.speed;
+				PlayerHitBox.y = player1.posy;
+			}
 		}
 		if (IsKeyDown(KEY_DOWN))
 		{
-			player1.posy += player1.speed;
-			PlayerHitBox.y = player1.posy;
+			if (player1.posy < (GetScreenHeight()) - (2 * (GetScreenHeight() / 18)))
+			{
+				player1.posy += player1.speed;
+				PlayerHitBox.y = player1.posy;
+			}
 		}
 
 		//Input Mouse
 
 		if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
 		{
-			player1.posx = GetMouseX();
-			player1.posy = GetMouseY();
-			PlayerHitBox.x = player1.posx;
-			PlayerHitBox.y = player1.posy;
+			
+				player1.posx = GetMouseX();
+				player1.posy = GetMouseY();
+				PlayerHitBox.x = player1.posx;
+				PlayerHitBox.y = player1.posy;
+			
 		}
 
 
 		//Collisiondetection
-		if (!CheckCollisionRecs(PlayerHitBox,Boundarys_innen))
+		if (!CheckCollisionRecs(PlayerHitBox, Boundarys_innen))
 		{
 			for (int i = 0; i < 10; i++) {
-				std::cout << "HIT"<<i<< std::endl;
+				std::cout << "HIT" << i << std::endl;
 			}
 		}
 
@@ -105,29 +116,24 @@ int main(int argc, char* argv[])
 
 		ClearBackground(RAYWHITE);
 		//GameLoop
-		DrawGrid(5, 100);
+	
 		for (int i = 0; i <= 32; i++)
 		{
-			
-
-				DrawRectangleLinesEx(Boundarys,(GetScreenHeight()/18), RED);
-				DrawRectangleLinesEx(PlayerHitBox, 10,BLUE);
 
 
-
-
-			
+			DrawRectangleLinesEx(Boundarys, (GetScreenHeight() / 18), RED);
+			DrawRectangleLinesEx(PlayerHitBox, 10, BLUE);
 
 		}
 		EndDrawing();
 		//----------------------------------------------------------------------------------
 
 	}
-			// De-Initialization
-			//--------------------------------------------------------------------------------------   
-		CloseWindow();        // Close window and OpenGL context
-		//--------------------------------------------------------------------------------------
+	// De-Initialization
+	//--------------------------------------------------------------------------------------   
+	CloseWindow();        // Close window and OpenGL context
+	//--------------------------------------------------------------------------------------
 
-		return 0;
+	return 0;
 
 }
